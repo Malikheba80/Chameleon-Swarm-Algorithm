73f76af75c1a66a717a3696b5a411f2d1e83139f
Malikheba80 <80470963+Malikheba80@users.noreply.github.com>
2021-08-21 15:03:34 +0300

    Update initialization.m

--- initialization.m
+++ initialization.m
@@ -1,4 +1,3 @@
-%_________________________________________________________________________________
 %% Chameleon Swarm Algorithm (CSA) source codes version 1.0
 %
 %  Developed in MATLAB R2018a
@@ -19,20 +18,44 @@
 % This function initialize the first population of chameleons
 function pos=initialization(searchAgents,dim,u,l)
 
-% number of boundaries
-Bound_no= size(u,1); 
+% This function initialize the first population of search agents
+Boundary_no= size(u,2); % numnber of boundaries
 
-% If the boundaries of all variables are equal, and user enters a signle number for both u and l
-if Bound_no==1
-    pos=rand(searchAgents,dim).*(u-l)+l;
+% If the boundaries of all variables are equal and user enter a signle
+% number for both u and l
+if Boundary_no==1
+    u_new=ones(1,dim)*u;
+    l_new=ones(1,dim)*l;
+else
+     u_new=u;
+     l_new=l;   
 end
 
-% If each variable has a different boundary l and u
-if Bound_no>1
+% If each variable has a different l and u
     for i=1:dim
-        u_i=u(i);
-        l_i=l(i);
+        u_i=u_new(i);
+        l_i=l_new(i);
         pos(:,i)=rand(searchAgents,1).*(u_i-l_i)+l_i;
     end
-end
+
+% pos=pos';
+
+
+% 
+% % number of boundaries
+% Bound_no= size(u,1); 
+% 
+% % If the boundaries of all variables are equal, and user enters a signle number for both u and l
+% if Bound_no==1
+%     pos=rand(searchAgents,dim).*(u-l)+l;
+% end
+% 
+% % If each variable has a different boundary l and u
+% if Bound_no>1
+%     for i=1:dim
+%         u_i=u(i);
+%         l_i=l(i);
+%         pos(:,i)=rand(searchAgents,1).*(u_i-l_i)+l_i;
+%     end
+% end
 
